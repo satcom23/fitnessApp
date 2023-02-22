05d72cd4caee85c5288bf6413650c836953e441d
Jimmy O <jimmyojeda94@gmail.com>
2023-02-21 21:51:53 -0800

    changed background color

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <App className="mainApp" />
   </BrowserRouter>
 );
